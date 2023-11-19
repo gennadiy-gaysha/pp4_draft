64c4521371a4f913dcedbffcf48d1a2f789097a9
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-11-19 10:47:57 +0100

    fdfdfdf

--- blog/filters.py
+++ blog/filters.py
@@ -17,7 +17,7 @@ class PostFilter(django_filters.FilterSet):
     country = django_filters.ModelChoiceFilter(
         queryset=Country.objects.all().order_by('country_name'),
         widget=forms.Select(attrs={'class': 'form-control', 'style': 'width: 20%; display: inline;'}),
-        label='Filter by country',
+
     )
 
     class Meta:
